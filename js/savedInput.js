e632733aee8974f9b76944a9fba363594d764ef2
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-11 18:16:06 +0100

    Start of edit input

--- js/savedInput.js
+++ js/savedInput.js
@@ -22,3 +22,35 @@ async function getInput(){
         })
     })
 }
+
+function editInput(){
+    let header = new Headers();
+    header.append("Content-Type", "application/json");
+
+    let inputPlace = {
+        input: inputPlace
+    }
+
+    fetch(`https://sterrenkijker.herokuapp.com/updateInput/:input`, {
+        method: 'PUT',
+        headers: header,
+        body: JSON.stringify(inputPlace)
+    })
+    .then(response => response.json())
+    .then(dataGet =>{
+        console.log("Succes Update", dataGet)
+        dataGet.forEach(saved => {
+            const containerSaved = document.getElementById('savedContainer');
+            const savedString =  `
+                <div id="savedPlaceContainer">
+                    <h2 id="savedInput">${saved.input}</h2>
+                    <div id="editDelete">
+                        <button id="background" class="fas fa-edit"></button>
+                        <button id="background2"><i class="fas fa-trash-alt"></i></span>
+                    </div>
+                </div>    
+            `;
+            containerSaved.insertAdjacentHTML('beforeend', savedString);
+        })
+    })
+}
