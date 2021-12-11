cf595c9f70e44d16cd0182f43f440eb7b9075011
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-11 16:32:28 +0100

    Standaard css voor saves

--- js/savedInput.js
+++ js/savedInput.js
@@ -10,11 +10,13 @@ async function getInput(){
         dataGet.forEach(saved => {
             const containerSaved = document.getElementById('savedContainer');
             const savedString =  `
-                <h2 id="savedInput">${saved.input}</h2>
-                <div id="editDelete">
-                    <span class="fas fa-edit"></span>
-                    <span class="fas fa-trash-alt"></span>
-                </div>
+                <div id="savedPlaceContainer">
+                    <h2 id="savedInput">${saved.input}</h2>
+                    <div id="editDelete">
+                        <span id="background"><i class="fas fa-edit"></i></span>
+                        <span id="background2"><i class="fas fa-trash-alt"></i></span>
+                    </div>
+                </div>    
             `;
             containerSaved.insertAdjacentHTML('beforeend', savedString);
         })
