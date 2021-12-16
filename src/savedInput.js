7d22469f56e787fdd03299e888a9d19ea7d1f178
mikederycke <mike.derycke@ehb.be>
2021-12-16 12:10:52 +0100

    Restructure input places

--- src/savedInput.js
+++ src/savedInput.js
@@ -1,13 +1,40 @@
-window.onload = getInput()
+let cityId;
 
-async function getInput(){
+window.onload = function () {
+    console.log('loaded');
+    getInput();
+
+    // Prep event listeners for submit
+    document.getElementById('editForm').addEventListener("submit", submitChange);
+    // Prep event listeners for clicks on icons
+    document.getElementById('savedContainer').addEventListener('click', (e) => {
+        console.log('click');
+
+        cityId = e.target.closest('.savedPlaceContainer').id;
+
+        // On EDIT
+        if (cityId && e.target.className.indexOf('edit') !== -1) {
+            console.log('Edit')
+            document.getElementById('editForm').style.display = "block";
+            //@TODO add current city name to inputfield   
+
+        } else if (cityId && e.target.className.indexOf('trash') !== -1) {
+            console.log('trash')
+        }
+
+    });
+}
+
+async function getInput() {
     const response = await fetch(`https://sterrenkijker.herokuapp.com/inputPlace`);
     const dataGet = await response.json();
-   
-        console.log("Succes Get", dataGet)
-        dataGet.forEach(saved => {
-            const containerSaved = document.getElementById('savedContainer');
-            const savedString =  `
+
+    // @TODO Clear the current list
+
+    console.log("Succes Get", dataGet)
+    const containerSaved = document.getElementById('savedContainer');
+    dataGet.forEach(saved => {
+        const savedString = `
             <div id="${saved._id}" class="savedPlaceContainer">
                 <h2 id="savedInput">${saved.input}</h2>
                 <div id="editDelete">
@@ -15,64 +42,36 @@ async function getInput(){
                     <button class="containerDelete"><i class="fas fa-trash-alt"></i></button>
                 </div>
 
-                <form id="editFrom" style="display: none">
-                    <h2>Change the city name</h2>
-                    <input id="editCity" type="text" name="name" placeholder="Name of city" />
-                    <button id="submitCity" type="submit">Send</button>
-                </form>
+                
 
             </div>`;
-            containerSaved.insertAdjacentHTML('beforeend', savedString);
-        });
-        editInput(dataGet);
+        containerSaved.insertAdjacentHTML('beforeend', savedString);
+    });
+
 }
 
-function editInput(dataGet){
+function submitChange(event) {
+    event.preventDefault();
+    console.log('edit the city');
 
-    // Hier ligt het probleem waar ik op het laatste van de les nog even over had gesproken.
-    // Het probleem waar ik tegen aan loop is:
-    // Ik kan de form aanspreken en kan iets ingeven als verandering van de gemeente/stad, maar als ik op send druk
-    // dan voert hij gelijk de getInput() uit en wordt er geen wijzeging doorgevoerd.
-    // Als ik dan ga kijken bij postman krijg ik mijn error te zien (file script.js(backend) op lijn 136).
-    // Zelf heb ik al wat proberen zoeken, maar ik kan niets vinden.
-    // Super bedankt dat je ernaar wilt kijken 
-    // Groetjes
+    // event.preventDefault();
+    //                 let newCityName = document.getElementById('editCity').value;
 
-    document.getElementById('savedContainer').addEventListener('click', (e) => {
-        
-        const cityId = e.target.closest('.savedPlaceContainer').id;
-        
-        document.getElementById('editFrom').style.display = "block";
-        if(cityId){
-            if(e.target.className.indexOf('edit') !== -1){
-                document.getElementById('submitCity').addEventListener("submit", e => {
-                    e.preventDefault();
-                    let newCityName = document.getElementById('editCity').value;
-                    
-                    let header = new Headers();
-                    header.append("Content-Type", "application/json");
+    //                 let header = new Headers();
+    //                 header.append("Content-Type", "application/json");
 
-                    fetch(`https://sterrenkijker.herokuapp.com/updateInput/:id`, {
-                        method: 'PUT',
-                        headers: header,
-                        body: JSON.stringify({
-                            input: newCityName
-                        })
-                    })
-                    .then(response => response.json())
-                    .then(dataPut =>{
-                        console.log("Succes Update", dataPut);
-                        setTimeout(document.getElementById('editFrom').style.display = "none", 2000)
-                        setTimeout(getInput, 1000)
-                    })
-                })
-                console.log('Edit')                
-            }
-            
-            if(e.target.className.indexOf('trash') !== -1){
-                console.log('trash')
-            }
-    
-        }
-    });        
+    //                 fetch(`https://sterrenkijker.herokuapp.com/updateInput/:id`, {
+    //                     method: 'PUT',
+    //                     headers: header,
+    //                     body: JSON.stringify({
+    //                         input: newCityName
+    //                     })
+    //                 })
+    //                 .then(response => response.json())
+    //                 .then(dataPut =>{
+    //                     console.log("Succes Update", dataPut);
+    //                     setTimeout(document.getElementById('editFrom').style.display = "none", 2000)
+    //                     setTimeout(getInput, 1000)
+    //                 })
+
 }
